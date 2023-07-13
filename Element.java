8a49944ac2e0d082e9de634df62cb9f717cb368a
INDUSAKTHIVEL <indusakthi2003@gmail.com>
2023-07-13 19:29:59 +0530

    Element

---
+++ Element.java
@@ -0,0 +1,27 @@
+package Array_element;
+public class Element {
+    public static int countElementsGreaterThan(int[] A) {
+        int n = A.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = A[i];
+            int greaterCount = 0;
+
+            for (int j = i + 1; j < n; j++) {
+                if (A[j] > num) {
+                    greaterCount++;
+                }
+            }
+
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5};
+        
+        int result = countElementsGreaterThan(A);
+        System.out.println( "Number of elements with at least one greater element: " + result);
+    }
+}
